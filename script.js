c1ebc622acc53c0deae293fb92c8c446879cc037
Tarek <114747789+Tarek124@users.noreply.github.com>
2024-03-03 02:18:09 +0600

    Let’s Discuss section done

---
+++ script.js
@@ -0,0 +1,92 @@
+const fetchData = () => {
+  const url = " https://openapi.programming-hero.com/api/retro-forum/posts";
+  const request = new Request(url);
+  fetch(request)
+    .then((response) => response.json())
+    .then((data) => displayPost(data))
+    .catch((err) => console.log(err));
+};
+fetchData();
+// show all post
+const displayPost = (data) => {
+  const displayCard = document.getElementById("displayCard");
+  let myData = data.posts;
+  for (const p of myData) {
+    const newDiv = document.createElement("div");
+    const dot = p.isActive ? "bg-green-700" : "bg-red-700";
+    newDiv.innerHTML = `   <div
+        class="bg-[#F3F3F5] flex sm:flex-row flex-col rounded-md box-border p-8 gap-4 mb-5 shadow-md"
+      >
+        <div>
+          <div class="avatar">
+          <div class="sm:w-[72px] sm:h-[72px] w-14 h-14 mask rounded-xl">
+              <div class=" border-white  absolute h-4 w-4 ${dot} -right-1 -top-1 rounded-full border"></div>
+              <img src=${p.image}>
+            </div>
+          </div>
+        </div>
+        <div class="w-full">
+          <div class="flex text-sm font-medium gap-4">
+            <p># ${p.category}</p>
+            <p>Author: ${p.author.name}</p>
+          </div>
+          <h1 class="text-2xl font-bold">
+            ${p.title}
+          </h1>
+          <p class="text-base py-1">
+           ${p.description}
+          </p>
+          <div class="border-t-[1px] border-dashed my-2"></div>
+          <div class="flex justify-between pt-2 ">
+            <div class="flex gap-4">
+              <div class="flex items-center gap-3">
+                <div><img src="./images/Vector.png" alt="" /></div>
+                <span>${p.comment_count}</span>
+              </div>
+              <div class="flex items-center gap-3">
+                <div><img src="./images/Vector (1).png" alt="" /></div>
+                <span>${p.view_count}</span>
+              </div>
+              <div class="flex items-center gap-3">
+                <div>
+                  <img
+                    src="./images/tabler-icon-clock-hour-9.png"
+                    alt=""
+                  />
+                </div>
+                <span>${p.posted_time} min</span>
+              </div>
+            </div>
+            <div>
+              <img
+              onclick="handleReadButton('${p.title}','${p.view_count}')"
+                class="cursor-pointer"
+                src="./images/email 1.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>`;
+    displayCard.appendChild(newDiv);
+  }
+
+  console.log(data);
+};
+
+const readMail = [];
+const handleReadButton = (data) => {
+  console.log(data);
+};
+// author: {
+//   name: "John Doe";
+// }
+// category: "Comedy";
+// comment_count: 560;
+// description: "It is one thing to subject yourself to a costume mishap";
+// id: 101;
+// image: "https://i.ibb.co/0QRxkd5/pexels-jan-kop-iva-3525908.jpg";
+// isActive: true;
+// posted_time: 5;
+// title: "10 Kids Unaware of Their Costume";
+// view_count: 1568;
